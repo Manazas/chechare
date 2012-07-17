f45676d6dbb43a93d29e0a87e202f81272e02ecb
Eduardo Lostal <eduardolostal@gmail.com>
2012-07-17 10:46:59 +0200

    First project upload

---
+++ js/main.js
@@ -0,0 +1,86 @@
+change_argentinian = function() {
+    $('#argentinian').show();
+    $('#spanish').hide();
+}
+
+change_spanish = function() {
+    $('#argentinian').hide();
+    $('#spanish').show();
+}
+
+show_gallery = function() {
+    $('#gallery').show();
+    $('#videos').hide();
+    $('#video_message').hide();
+}
+
+show_videos = function() {
+    $('#gallery').hide();
+    $('#videos').show();
+    $('#video_message').show();
+    show_video1();
+}
+
+show_video1 = function() {
+    $('#video1').show();
+    $('#video2').hide();
+    $('#video3').hide();
+    $('#video4').hide();
+    $('#video5').hide();
+    $('#video6').hide();
+}
+
+show_video2 = function() {
+    $('#video1').hide();
+    $('#video2').show();
+    $('#video3').hide();
+    $('#video4').hide();
+    $('#video5').hide();
+    $('#video6').hide();
+}
+
+show_video3 = function() {
+    $('#video1').hide();
+    $('#video2').hide();
+    $('#video3').show();
+    $('#video4').hide();
+    $('#video5').hide();
+    $('#video6').hide();
+}
+
+show_video4 = function() {
+    $('#video1').hide();
+    $('#video2').hide();
+    $('#video3').hide();
+    $('#video4').show();
+    $('#video5').hide();
+    $('#video6').hide();
+}
+
+show_video5 = function() {
+    $('#video1').hide();
+    $('#video2').hide();
+    $('#video3').hide();
+    $('#video4').hide();
+    $('#video5').show();
+    $('#video6').hide();
+}
+
+show_video6 = function() {
+    $('#video1').hide();
+    $('#video2').hide();
+    $('#video3').hide();
+    $('#video4').hide();
+    $('#video5').hide();
+    $('#video6').show();
+}
+
+change_revista = function() {
+    $('#revista').show();
+    $('#seventies').hide();
+}
+
+change_seventies = function() {
+    $('#revista').hide();
+    $('#seventies').show();
+}
